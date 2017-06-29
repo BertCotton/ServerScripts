7c2bfa46cf6371984419cdb910164b7abc9e1b26
Bert Cotton <RobertCotton@ius.gov>
2017-06-29 12:51:34 -0600

    updating speed test

--- speedtest.py
+++ speedtest.py
@@ -1,38 +1,16 @@
-import re
-import subprocess
-import graphitesend
-import datetime
-import time
+import speedtest
 
-print "Speed test started %s " % datetime.datetime.now()
+servers = []
+# If you want to test against a specific server
+# servers = [1234]
 
-start = time.time()
-try:
-	print "Starting speedtest"
-	speedtest = subprocess.Popen(["/mnt/scripts/speedtest-linux-amd64-v1.0.2-c"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	#speedtest = subprocess.Popen(["cat", "speedtest.log"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	print "reading from pipe"
-	out, error = speedtest.communicate()
-	g = graphitesend.init(prefix="speedtest", system_name='')
-	if out:
-		ping = float(re.findall(r"Ping \(Lowest\): (\d+\.\d*)", out)[0])
-		print "Latency %s ms" % ping
-		g.send("ping",ping)
+s = speedtest.Speedtest()
+s.get_servers(servers)
+s.get_best_server()
+s.download()
+s.upload()
+s.share()
 
-		download = float(re.findall(r"Download \(Max\): (\d+\.\d*)", out)[0])
-		print "Download Speed %s Mbit/s" % download
-		g.send("download",download)
+results_dict = s.results.dict()
 
-		upload = float(re.findall(r"Upload \(Max\): (\d+\.\d*)", out)[0])
-		print "Upload Speed %s Mbit/s" % upload
-		g.send("upload",upload)
-	else:
-		print "No output from test: %s" % error
-
-except Exception,e:
-	print str(e)
-
-end = time.time()
-duration = (end - start)
-print "Speed test finished %s " % datetime.datetime.now()
-print "Duration %s " % duration
+print(results_dict)
